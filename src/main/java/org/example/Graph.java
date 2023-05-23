5b1b076a3a085aebb00b8f74928f7351602b58d9
Geoffrey Otieno <otienogeoffrey812@gmai.com>
2023-05-23 21:15:35 +0300

    Added addNode, addEdge and print graph methods

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,14 +1,47 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Graph {
     private class Node{
-        private Node leftChild;
-        private Node rightChild;
         private String label;
         private Node(String label){
             this.label = label;
         }
+        @Override
+        public String toString(){
+            return label;
+        }
     }
     private Node root;
+    HashMap<String, Node> nodes = new HashMap<>();
+    HashMap<Node, List<Node>> adjacencyList = new HashMap<>();
+
+    public void addNode(String label){
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(String from, String to){
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+
+        if (fromNode == null || toNode == null){
+            throw new IllegalArgumentException();
+        }
+        adjacencyList.get(fromNode).add(toNode);
+    }
+
+    public void print(){
+        for (var source: adjacencyList.keySet()){
+            var target = adjacencyList.get(source);
+            if (!target.isEmpty()) {
+                System.out.println(source + " is connected to " + target);
+            }
+        }
+    }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Graph graph = new Graph();
+    graph.addNode("KSM");
+    graph.addNode("NBO");
+    graph.addNode("MSA");
+    graph.addNode("KLF");
+    graph.addEdge("NBO", "KSM");
+    graph.addEdge("NBO", "MSA");
+    graph.addEdge("KSM", "MSA");
+    graph.addEdge("MSA", "KLF");
+    graph.print();
     }
 }
